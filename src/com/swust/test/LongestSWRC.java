13d59db0b47d472326424fcf1489be005337c0df
wenhao <88888@qq.com>
2020-04-28 23:19:19 +0800

    寻找最长子字符串

--- src/com/swust/test/LongestSWRC.java
+++ src/com/swust/test/LongestSWRC.java
@@ -14,6 +14,10 @@ import java.util.Set;
 * String.length()  获得字符串的长度
 * int Math.man(a,b)   返回a，b之间的最大值
 * Set 的remove 方法 可以移除对应的值
+*
+* 那么为什么说是未优化的?
+* 因为当发现end指针的内容和窗口里的重复时，start的指针要逐个往end靠拢，时间复杂度为O(2n)
+* 优化后的，把位置存入每个字符的位置信息存入滑动窗口中，当发现end重复后，直接可以按内容去除掉该重复，并把start指针移动到它的位置上
 * */
 /**
  * @author WENHAO
@@ -25,11 +29,14 @@ public class LongestSWRC {
         int end = 0;
         Set s1 = new HashSet();
         int res = 0;
-        while (start<s.length()&&end<s.length()){
-            if(!s1.contains(s.charAt(end))){
+
+        while (start<s.length()&&end<s.length()){//该条件判断窗口是否滑完整个字符串
+            if(!s1.contains(s.charAt(end))){  //看当前指针指向的字符，在窗口字符里是否包含
+                //如果不包含，把当前指针指向的字符添加到滑动窗口里
                 s1.add(s.charAt(end++));
+                //统计滑动窗口里的字符数量，并把他存进去
                 res = Math.max(res,end-start);
-            }else
+            }else//如果包含，移除掉当前start指针指向的字符，并且start指针右移
                 {
                 s1.remove(s.charAt(start++));
             }
